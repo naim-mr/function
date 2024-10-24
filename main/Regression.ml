e5d174ae893d0d56ac4307f294d17c9de3efe9e8
naim-mr <naim.moussaoui-remil@ens-rennes.fr>
2024-10-24 18:08:02 +0200

    Function diff taken from Mopsa diff + logs

--- main/Regression.ml
+++ main/Regression.ml
@@ -6,22 +6,23 @@ let create_logfile_name () =
   let name = name^"-domain"^(!domain)^(if !ordinals then "-ordinals"^(string_of_int !Ordinals.max) else "")^(if !refine then "-refine" else "")^".out" in
   Config.logfile := name
 
-let json_string filename time analysis property  domain    =
+let json_string filename time analysis property  domain  time =
    Printf.sprintf {|
   {"filename" : "%s",
    "analysis_type": "%s",
    "property": "%s",
    "result": "%s",
-   "domain": "%s"
-    }|} 
-    filename analysis property domain  
+   "domain": "%s",
+   "time": "%s"
+    }|}  
+    filename analysis property domain time
 
 let output_json () =   
   let replace =  Str.regexp ("tests/[A-Za-z]+/") in
   let basefile = Str.replace_first (replace) ((!output_dir)^(!analysis)^"/") (!filename) in
   let basefile = basefile^"-domain"^(!domain)^(if !ordinals then "-ordinals"^(string_of_int !Ordinals.max) else "")^(if !refine then "-refine" else "") in 
   let jsonfile = basefile^".json" in
-  let output =  json_string !filename time !analysis !property (if !Config.result then "TRUE" else "UKNOWN") !domain  in
+  let output =  json_string !filename time !analysis !property (if !Config.result then "TRUE" else "UKNOWN") !domain  !exectime in
   let json : Yojson.Safe.t =  `Assoc(("Config",Yojson.Safe.from_string output)::[("tree", !tree)]) in 
   let f = Out_channel.open_bin (jsonfile) in
   Yojson.Safe.pretty_to_channel f json;
