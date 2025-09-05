2b10c24e7e47668f4a4a8d15809f3603a387282b
naim-mr <naim.moussaoui-remil@ens-rennes.fr>
2025-09-05 10:58:30 +0200

    Folder org & clean

--- tests/termination/tacas2013c.c
+++ tests/termination/tacas2013c.c
@@ -12,9 +12,9 @@ int main() {
   while (x1 > 0 && x2 > 0 && x3 > 0)
     if (?) {
       x1 = x1 - 1;
-      x3 = rand();
+      x3 = ?;
     } else {
-      x1 = rand();
+      x1 = ?;
       x2 = x2 - 1;
       x3 = x3 - 1;
     }
